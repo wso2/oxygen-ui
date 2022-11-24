d1519ce00b0cb7ebf6d80fb773cf649fe0acbbc8
Brion <info@brionmario.com>
2022-11-25 02:22:41 +0530

    chore(scss): refactor scss package

---
+++ packages/oxygen-scss/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "testEnvironment": "node",
+  "testPathIgnorePatterns": [
+    "/node_modules/",
+    "/__tests__/utils/",
+    "/__tests__/docs.test.js"
+  ],
+  "transform": {}
+}
