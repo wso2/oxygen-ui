3d042257138d1b2cb11a4df90540a5d84f213cfb
Brion <info@brionmario.com>
2022-11-29 14:16:00 +0530

    chore(react): add a basic rollup config

---
+++ packages/oxygen-react/rollup.config.cjs
@@ -0,0 +1,43 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const dts = require('rollup-plugin-dts');
+const postcss = require('rollup-plugin-postcss');
+const {terser} = require('rollup-plugin-terser');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const image = require('@rollup/plugin-image');
+
+const pkg = require('./package.json');
+
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({tsconfig: './tsconfig.lib.json'}),
+      postcss(),
+      terser(),
+      image(),
+    ],
+  },
+  {
+    external: [/\.css$/],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'esm'}],
+    plugins: [dts.default()],
+  },
+];
--- packages/oxygen-react/src/components/index.ts
+++ packages/oxygen-react/src/components/index.ts
@@ -1,2 +1,15 @@
-export * from './core';
-export * from './patterns';
+// Named exports enable users to import the modules
+// like `import { Button } from '@oxygen/react';`
+// other than `import Button from '@oxygen/react/Button';`
+
+export {default as Button} from './Button';
+export * from './Button';
+
+export {default as Link} from './Link';
+export * from './Link';
+
+export {default as SignIn} from './SignIn';
+export * from './SignIn';
+
+export {default as TextField} from './TextField';
+export * from './TextField';
