c1f3de5ccaaee280632c52305167840e18b6e11c
Brion <info@brionmario.com>
2022-11-29 15:48:34 +0530

    chore(react): fix tsconfig

--- packages/oxygen-react/src/components/index.ts
+++ packages/oxygen-react/src/components/index.ts
@@ -1,6 +1,8 @@
 // Named exports enable users to import the modules
 // like `import { Button } from '@oxygen/react';`
 // other than `import Button from '@oxygen/react/Button';`
+// TODO: Need to add a module `package.json` for the above `@oxygen/react/Button` syntax to work.
+// https://github.com/mui/material-ui/blob/master/scripts/copyFiles.mjs#L28
 
 export {default as Button} from './Button';
 export * from './Button';
