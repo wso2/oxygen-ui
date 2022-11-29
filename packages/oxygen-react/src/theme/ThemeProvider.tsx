e421a8b712a8297a42f3ae6d4d8b44e4a3f35fa5
Brion <info@brionmario.com>
2022-11-29 19:46:02 +0530

    chore(react): fix theme provider types

--- packages/oxygen-react/src/theme/ThemeProvider.tsx
+++ packages/oxygen-react/src/theme/ThemeProvider.tsx
@@ -1,16 +1,43 @@
-import React, {ReactElement} from 'react';
+import React, {PropsWithChildren, ReactElement} from 'react';
 import {Experimental_CssVarsProvider as CssVarsProvider} from '@mui/material/styles';
 import {CssBaseline, StyledEngineProvider} from '@mui/material';
-import {ThemeProviderProps as MuiThemeProviderProps} from '@mui/material/styles/ThemeProvider';
+import {CssVarsProviderConfig} from '@mui/system';
 import defaultTheme from './default-theme';
 
-export type ThemeProviderProps = Partial<MuiThemeProviderProps>;
+export type ColorScheme = 'light' | 'dark';
 
-const ThemeProvider = (props: ThemeProviderProps): ReactElement => {
+export type ThemeProviderProps = Partial<CssVarsProviderConfig<ColorScheme>> & {
+  /**
+   * The node used to attach the color-scheme attribute
+   * @default document
+   */
+  colorSchemeNode?: Document | HTMLElement | null;
+  /**
+   * The CSS selector for attaching the generated custom properties
+   * @default ':root'
+   */
+  colorSchemeSelector?: string;
+  /**
+   * The document used to perform `disableTransitionOnChange` feature
+   * @default document
+   */
+  documentNode?: Document | null;
+  /**
+   * The window that attaches the 'storage' event listener
+   * @default window
+   */
+  storageWindow?: Window | null;
+  theme?: {
+    colorSchemes: Record<ColorScheme, Record<string, any>>;
+    cssVarPrefix?: string;
+  };
+};
+
+const ThemeProvider = (props: PropsWithChildren<ThemeProviderProps>): ReactElement => {
   const {children, theme, ...rest} = props;
   return (
     <StyledEngineProvider injectFirst>
-      <CssVarsProvider theme={(theme as any) ?? defaultTheme} {...rest}>
+      <CssVarsProvider theme={theme ?? defaultTheme} {...rest}>
         <CssBaseline />
         {children}
       </CssVarsProvider>
