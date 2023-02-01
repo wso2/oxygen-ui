c71529f3ba0d7d00cabfe4b7e2fc5fcc25a2b419
Brion <info@brionmario.com>
2023-02-02 02:09:39 +0530

    chore(primitives): groundwork for SVG processing

--- packages/primitives/scripts/build.js
+++ packages/primitives/scripts/build.js
@@ -21,78 +21,13 @@ const fs = require('fs');
 const path = require('path');
 const StyleDictionary = require('style-dictionary');
 const { logger } = require('@oxygen-ui/logger');
+const cp = require('child_process');
 
-const PATHS = {
-  source: {
-    tokens: path.resolve(path.join(__dirname, '..', 'src', 'design-tokens')),
-  },
-};
-
-StyleDictionary.registerTransformGroup({
-  name: 'tokens-es',
-  transforms: ['name/cti/pascal', 'size/px', 'color/hex'],
-});
-
-StyleDictionary.registerTransformGroup({
-  name: 'tokens-scss',
-  transforms: ['name/cti/kebab', 'time/seconds', 'size/px', 'color/css'],
-});
-
-StyleDictionary.registerTransformGroup({
-  name: 'tokens-css',
-  transforms: ['name/cti/kebab', 'time/seconds', 'size/px', 'color/css'],
-});
-
-const getStyleDictionaryConfig = (brand, source) => {
-  const sourceFileName = path.basename(source);
-
-  return {
-    platforms: {
-      'web/css': {
-        buildPath: `dist/design-tokens/web/${brand}/css/`,
-        files: [
-          {
-            destination: sourceFileName.replace('.json', '.css'),
-            format: 'css/variables',
-          },
-        ],
-        prefix: brand,
-        transformGroup: 'tokens-css',
-      },
-      'web/es': {
-        buildPath: `dist/design-tokens/web/${brand}/es/`,
-        files: [
-          {
-            destination: sourceFileName.replace('.json', '.d.ts'),
-            format: 'typescript/es6-declarations',
-          },
-          {
-            destination: sourceFileName.replace('.json', '.js'),
-            format: 'javascript/module-flat',
-          },
-          {
-            destination: sourceFileName.replace('.json', '.es6.js'),
-            format: 'javascript/es6',
-          },
-        ],
-        prefix: brand,
-        transformGroup: 'tokens-es',
-      },
-      'web/scss': {
-        buildPath: `dist/design-tokens/web/${brand}/scss/`,
-        files: [
-          {
-            destination: sourceFileName.replace('.json', '.scss'),
-            format: 'scss/variables',
-          },
-        ],
-        prefix: brand,
-        transformGroup: 'tokens-scss',
-      },
-    },
-    source: [source],
-  };
-};
+// const PATHS = {
+//   source: {
+//     tokens: path.resolve(path.join(__dirname, '..', 'src', 'design-tokens')),
+//   },
+// };
 
 /* ====================================================================================== */
 /* Execution starts from here                                                             */
@@ -100,22 +35,13 @@ const getStyleDictionaryConfig = (brand, source) => {
 
 logger.log('=======================  🧱 Started Building Primitives 🧱  =======================');
 logger.log();
-logger.log('                         💅  Building Style Dictionary  💅  =======================');
+logger.log('                         💅  Building Style Dictionary  💅                         ');
 logger.log();
 
-fs.readdirSync(PATHS.source.tokens)
-  .forEach((brand) => {
-    logger.info(`Processing the Brand: [ ${brand} ]`);
+// cp.fork(path.resolve(__dirname, 'build-sd.js'));
 
-    fs.readdirSync(path.join(PATHS.source.tokens, brand))
-      .forEach((tokenFile) => {
-        const filePath = path.join(PATHS.source.tokens, brand, tokenFile);
+logger.log();
+logger.log('                          💅  Building the SVG Icons  💅                           ');
+logger.log();
 
-        const StyleDictionaryExtended = StyleDictionary
-          .extend(getStyleDictionaryConfig(brand, filePath));
-
-        StyleDictionaryExtended.buildPlatform('web/es');
-        StyleDictionaryExtended.buildPlatform('web/css');
-        StyleDictionaryExtended.buildPlatform('web/scss');
-      });
-  });
+cp.fork(path.resolve(__dirname, 'build-icons.js'));
