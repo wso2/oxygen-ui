fce6da42d206b5e2fd36642c3e98cb6b0d3f1dc7
savindi7 <savindi@wso2.com>
2023-02-22 18:35:22 +0530

    chore(react): improve Password type `TextField`

--- packages/react/src/components/TextField/TextField.tsx
+++ packages/react/src/components/TextField/TextField.tsx
@@ -16,29 +16,108 @@
  * under the License.
  */
 
-import {InputLabel} from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
 import MuiTextField, {TextFieldProps as MuiTextFieldProps} from '@mui/material/TextField';
+import {DoubleCircleIcon, VisibilityIcon, VisibilityOffIcon} from '@oxygen-ui/react-icons';
 import clsx from 'clsx';
-import {FC, ReactElement} from 'react';
+import {FC, MouseEvent, ReactElement, ReactNode, useState} from 'react';
 import {WithWrapperProps} from '../../models';
 import {composeComponentDisplayName} from '../../utils';
+import IconButton from '../IconButton';
+import InputLabel from '../InputLabel';
+import List from '../List';
+import ListItem from '../ListItem';
+import ListItemIcon from '../ListItemIcon';
+import ListItemText from '../ListItemText';
+import Tooltip from '../Tooltip';
 import './text-field.scss';
 
-export type TextFieldProps = MuiTextFieldProps;
+export type TextFieldProps = {
+  /**
+   * Criteria for user input.
+   */
+  criteria?: string[];
+} & MuiTextFieldProps;
 
 const COMPONENT_NAME: string = 'TextField';
 
 const TextField: FC<TextFieldProps> & WithWrapperProps = (props: TextFieldProps): ReactElement => {
-  const {className, label, InputLabelProps, id, ...rest} = props;
+  const {className, criteria, InputLabelProps, id, label, type, ...rest} = props;
 
   const classes: string = clsx('oxygen-text-field', className);
 
+  const [open, setOpen] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = (): void => {
+    if (type === 'password' && criteria?.length > 0) {
+      setOpen(true);
+    }
+  };
+
+  const handleClose = (): void => {
+    if (open) {
+      setOpen(false);
+    }
+  };
+
+  const handleClickShowPassword = (): void => setShowPassword((show: boolean) => !show);
+
+  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+  };
+
+  const tooltipContent = (): ReactNode => (
+    <List>
+      {criteria?.map((criterion: string) => (
+        <ListItem disablePadding>
+          <ListItemIcon>
+            <DoubleCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary={criterion} />
+        </ListItem>
+      ))}
+    </List>
+  );
+
   return (
     <div className={classes}>
-      <InputLabel htmlFor="id" shrink className="oxygen-text-field__label" {...InputLabelProps}>
+      <InputLabel htmlFor={id} aria-describedby={id} {...InputLabelProps}>
         {label}
       </InputLabel>
-      <MuiTextField className="oxygen-text-field__input" id={id} {...rest} />
+      <Tooltip
+        arrow
+        placement="right-end"
+        describeChild
+        open={open}
+        title={tooltipContent()}
+        classes={{arrow: 'oxygen-text-field-tooltip-arrow', tooltip: 'oxygen-text-field-tooltip'}}
+      >
+        <MuiTextField
+          id={id}
+          type={type === 'password' && (showPassword ? 'text' : 'password')}
+          InputProps={
+            type === 'password' && {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }
+          }
+          onClick={handleClick}
+          onBlurCapture={handleClose}
+          onFocus={handleClick}
+          {...rest}
+        />
+      </Tooltip>
     </div>
   );
 };
