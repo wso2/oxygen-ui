9516c5fa0c18e2c8da9c011e6090126949dec4a5
Brion Mario <brionbmp@gmail.com>
2025-02-07 14:14:02 +0530

    chore: refactor input props in `PasswordField`

--- packages/react/src/components/TextField/TextField.tsx
+++ packages/react/src/components/TextField/TextField.tsx
@@ -56,7 +56,7 @@ export type TextFieldProps<C extends ElementType = ElementType> = {
 
 const PasswordField: ForwardRefExoticComponent<TextFieldProps> = forwardRef(
   <C extends ElementType = ElementType>(
-    {type, variant, InputProps = {}, ...rest}: TextFieldProps<C>,
+    {type, variant, InputProps, ...rest}: TextFieldProps<C>,
     ref: Ref<HTMLDivElement>,
   ): ReactElement => {
     const [showPassword, setShowPassword] = useState(false);
@@ -72,8 +72,8 @@ const PasswordField: ForwardRefExoticComponent<TextFieldProps> = forwardRef(
         ref={ref}
         type={showPassword ? 'text' : 'password'}
         InputProps={{
-          ...InputProps,
-          endAdornment: InputProps.endAdornment || (
+          ...(InputProps ?? {}),
+          endAdornment: InputProps?.endAdornment || (
             <InputAdornment position="end">
               <IconButton
                 aria-label="toggle password visibility"
