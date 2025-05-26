836483d23aa817257481eb0513469bb26b2e9aaf
DonOmalVindula <omalvindula@live.com>
2025-05-26 15:22:57 +0530

    chore:fix lint failure

--- examples/multi-brand-identity/src/branding/themes/DefaultTheme.ts
+++ examples/multi-brand-identity/src/branding/themes/DefaultTheme.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2022-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -12,6 +12,7 @@
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
  * under the License.
  */
 
