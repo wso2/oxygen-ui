6ea2345e7b983e7099ef7d6b4e95f1f8f7bcf02f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2026-07-11 07:03:03 +0530

    style(docs): restore border and shadow on Modal story surfaces

--- packages/oxygen-ui-docs/stories/Utils/Modal.stories.tsx
+++ packages/oxygen-ui-docs/stories/Utils/Modal.stories.tsx
@@ -52,6 +52,9 @@ const modalStyle = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
   p: 4,
 };
 
