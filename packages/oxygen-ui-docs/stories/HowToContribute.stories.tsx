292339a86da74a8219f83c7976e6f7eb94cf867b
Dushmilan <Dushmilan05@gmail.com>
2026-09-16 19:41:14 +0530

    test(oxygen-ui): link Testing contract path to published doc

--- packages/oxygen-ui-docs/stories/HowToContribute.stories.tsx
+++ packages/oxygen-ui-docs/stories/HowToContribute.stories.tsx
@@ -18,7 +18,7 @@
 
 import {Description, Primary, Subtitle, Title} from '@storybook/addon-docs/blocks';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Stack, Typography, CodeBlock, Box, Divider } from '@wso2/oxygen-ui';
+import { Stack, Typography, CodeBlock, Box, Divider, Link } from '@wso2/oxygen-ui';
 import React from 'react';
 import CenterContentLayout from '../layouts/CenterContentLayout';
 
@@ -602,7 +602,15 @@ export const TestingContract: Story = {
   render: () => (
     <CenterContentLayout>
       <Typography variant="body2" color="text.secondary">
-        Colocated unit tests follow <code>packages/oxygen-ui/TESTING.md</code>.
+        Colocated unit tests follow{' '}
+        <Link
+          href="https://github.com/wso2/oxygen-ui/blob/main/packages/oxygen-ui/TESTING.md"
+          target="_blank"
+          rel="noopener"
+        >
+          <code>packages/oxygen-ui/TESTING.md</code>
+        </Link>
+        .
       </Typography>
     </CenterContentLayout>
   ),
